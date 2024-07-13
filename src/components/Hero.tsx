a0489f4c4b5f748e4f396acad56b51dc1ff1040f
KamoEllen <108501662+KamoEllen@users.noreply.github.com>
2024-07-13 14:22:45 +0200

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,21 +4,22 @@ export const Hero = () => {
   return (
     <section className="container grid lg:grid-cols-2 place-items-center py-20 md:py-32 gap-10">
       <div className="text-center lg:text-start space-y-6">
-        <main className="text-5xl md:text-6xl font-bold">
+         <main className="text-5xl md:text-6xl font-bold">
           <h1 className="inline">
-            <span className="inline bg-gradient-to-r from-green-400 to-blue-500 text-transparent bg-clip-text">
+            <span className="inline bg-gradient-to-r from-green-500 to-green-200 text-transparent bg-clip-text">
               Ready 
             </span>{" "}
-            to make 
+            to  
           </h1>{" "}
-          script supervision{" "}
           <h2 className="inline">
-            <span className="inline bg-gradient-to-r from-green-400 via-green-500 to-blue-500 text-transparent bg-clip-text">
-              simpler
+            <span className="inline bg-gradient-to-r from-green-500 to-green-200 text-transparent bg-clip-text">
+              elevate
             </span>{" "}
-            and more ? {" "}
+            your script supervision  {" "}
+            <span className="text-green-500 underline" >experience?</span>
           </h2>
         </main>
+        
 
         <p className="text-xl text-muted-foreground md:w-10/12 mx-auto lg:mx-0 pb-8 lg:pb-12">
           From import to export, editing to automatic form filling, The Script App empowers you to manage scripts and continuity reports with ease, saving you time and effort.
