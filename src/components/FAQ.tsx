8388395af75c0fdac868aa31cc17202a6fe80081
KamoEllen <108501662+KamoEllen@users.noreply.github.com>
2024-07-17 13:54:50 +0200

    Update FAQ.tsx

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -14,32 +14,32 @@ interface FAQProps {
 const FAQList: FAQProps[] = [
   {
     question: "What features does The Script App offer?",
-    answer: "The Script App offers a variety of features designed to simplify the life of script supervisors. These include importing scripts, editing them, creating pages, auto-filling forms, and exporting or sending scripts via email. It's all interconnected to eliminate the need for manual retyping and streamline your workflow.",
+    answer: "The Script App offers a variety of features designed to simplify the life of script supervisors. These include importing scripts, adding tramlines, auto-filling aspects of the continuity reports, and exporting or sending scripts via email. It's all interconnected to eliminate manual retyping and help streamline your workflow.",
     value: "item-1",
   },
   {
     question: "How does the 24-hour free trial work?",
     answer:
-      "You can enjoy a 24-hour free trial of The Script App to explore all its features. During this period, you can import scripts, edit them, and use all functionalities without any charge. If you decide to continue using Script App after the trial, you can subscribe for only £20 per month.",
+      "You can enjoy a 24-hour free trial of The Script App to explore all its features. During this period, you can import scripts, edit them, and use all functionalities without any charge.This is perfect for dailies.If you decide to continue using The Script App after the free trial, you can subscribe for only £20 per month and cancel within the first 30 days of each month.",
     value: "item-2",
   },
   {
     question:
       "How did The Script App come to be?",
     answer:
-      "With extensive experience in the production industry, we recognized the challenges of script management and the need for accuracy and efficiency. This led us to create The Script App, designed to elevate workflow and save time on administrative tasks.",
+      "With extensive experience in the script supervising and Film & TV industry, we recognized the constant struggle of a work and life balance.This led us to create The Script App, designed to elevate workflow and save time on administrative tasks.",
     value: "item-3",
   },
   {
     question: "How do I pay for The Script App?",
-    answer: "Payment for Script App is handled through a secure online payment system. You can subscribe using a credit card or other accepted payment methods. Payments are processed monthly at the rate of £20 per month.",
+    answer: "Payment for Script App is handled through a secure online payment system. You can subscribe using a credit card or other accepted payment methods. Payments of £20 are processed monthly on the same date as your initial purchase date each month.",
     value: "item-4",
   },
   {
     question:
       "Can I suggest new features for The Script App?",
     answer:
-      "Absolutely! We value your feedback and suggestions for improving The Script App. If you have ideas for new features or improvements, please reach out to our support team. We are committed to improving The Script App based on user input.",
+      "Absolutely! We are always looking for new ways to improve The Script App and would love to hear your feedback.If you have ideas for new features or improvements, please reach out to our support team. We&amp;re here to listen",
     value: "item-5",
   },
   
@@ -81,7 +81,7 @@ export const FAQ = () => {
     </section>
   );
 };
-
+{/* we recognized the challenges of script management and the need for accuracy and efficiency.*/}
 {/*
 
 import {
