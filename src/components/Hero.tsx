d726b929d2651fdf8eec1a0fa2794e2c76250c01
KamoEllen <108501662+KamoEllen@users.noreply.github.com>
2024-07-17 16:08:55 +0200

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,13 +6,13 @@ export const Hero = () => {
       <div className="text-center lg:text-start space-y-6">
          <main className="text-5xl md:text-6xl font-bold">
           <h1 className="inline">
-            <span className="inline bg-gradient-to-r from-green-400 to-green-500 text-transparent bg-clip-text">
+            <span className="inline bg-gradient-to-r from-green-300 to-green-500 text-transparent bg-clip-text">
               Ready 
             </span>{" "}
             to  
           </h1>{" "}
           <h2 className="inline">
-            <span className="inline bg-gradient-to-r from-green-400 to-green-500 text-transparent bg-clip-text">
+            <span className="inline bg-gradient-to-r from-green-300 to-green-500 text-transparent bg-clip-text">
               type less
             </span>{" "}
             and  {" "}
