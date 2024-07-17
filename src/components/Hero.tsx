68b27ad52ee81da8df195ec44fdd1733dbbafaa7
KamoEllen <108501662+KamoEllen@users.noreply.github.com>
2024-07-17 13:15:58 +0200

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -48,58 +48,5 @@ export const Hero = () => {
       <div className="shadow"></div>
     </section>
   );
-};
-
-{/*
-import Newsletter from "./Newsletter";
-
-export const Hero = () => {
-  return (
-    <section className="container grid lg:grid-cols-2 place-items-center py-20 md:py-32 gap-10">
-      <div className="text-center lg:text-start space-y-6">
-         <main className="text-5xl md:text-6xl font-bold">
-          <h1 className="inline">
-            <span className="inline bg-gradient-to-r from-green-500 to-green-200 text-transparent bg-clip-text">
-              Ready 
-            </span>{" "}
-            to  
-          </h1>{" "}
-          <h2 className="inline">
-            <span className="inline bg-gradient-to-r from-green-500 to-green-200 text-transparent bg-clip-text">
-              elevate
-            </span>{" "}
-            your script supervision  {" "}
-            <span className="text-green-500 underline" >experience?</span>
-          </h2>
-        </main>
-        
-
-        <p className="text-xl text-muted-foreground md:w-10/12 mx-auto lg:mx-0 pb-8 lg:pb-12">
-         From importing scripts to exporting reports and handling edits to automating form filling.The Script App respects your dedication by offering efficient management tools, saving you valuable time.
-        </p>
-
-       
-      </div>
-
-      
  
-      <div className="z-10">
-        
-      <h2 className="text-3xl md:text-4xl font-bold pl-5">
-          Join our 
-          <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
-            {" "}
-            Waiting list{" "}
-          </span>
-          for early access
-        </h2>
-      <Newsletter/>
-      </div>
-      
-
-     
-      <div className="shadow"></div>
-    </section>
-  );
-  */}
 };
